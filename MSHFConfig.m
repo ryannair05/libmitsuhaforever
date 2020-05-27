99c2841c3ba99f2bb5ebad2604dc8d8a974358f4
Conor Byrne <con0r.b@protonmail.com>
2020-05-27 03:31:00 +0100

    Use DarwinNotificationsManager

--- MSHFConfig.m
+++ MSHFConfig.m
@@ -3,13 +3,16 @@
 #import <Cephei/HBPreferences.h>
 #import <ConorTheDev/libconorthedev.h>
 
-void notificationCallback(CFNotificationCenterRef center, void *observer,
-                          CFStringRef name, void const *object,
-                          CFDictionaryRef userInfo) {
-  NSLog(@"[MitsuhaForever] prefs changed");
-  MSHFConfig *ob = (MSHFConfig *)CFBridgingRelease(observer);
-  [ob reloadConfig];
-}
+@interface DarwinNotificationsManager : NSObject
+
+@property (strong, nonatomic) id someProperty;
+
++ (instancetype)sharedInstance;
+
+- (void)registerForNotificationName:(NSString *)name callback:(void (^)(void))callback;
+- (void)postNotificationWithName:(NSString *)name;
+
+@end
 
 @implementation MSHFConfig
 
@@ -17,11 +20,9 @@ void notificationCallback(CFNotificationCenterRef center, void *observer,
   self = [super init];
   if (self) {
     [self setDictionary:dict];
-    CFNotificationCenterAddObserver(
-        CFNotificationCenterGetDarwinNotifyCenter(),
-        (__bridge const void *)(self),
-        (CFNotificationCallback)notificationCallback,
-        (CFStringRef)MSHFPreferencesChanged, NULL, kNilOptions);
+    [[DarwinNotificationsManager sharedInstance] registerForNotificationName:MSHFPreferencesChanged callback:^() {
+      [self reloadConfig];
+    }];
   }
   return self;
 }
@@ -222,5 +223,64 @@ void notificationCallback(CFNotificationCenterRef center, void *observer,
   return [[MSHFConfig alloc]
       initWithDictionary:[MSHFConfig parseConfigForApplication:name]];
 }
+
+@end
+
+@implementation DarwinNotificationsManager {
+    NSMutableDictionary * handlers;
+}
+
++ (instancetype)sharedInstance {
+    static id instance = NULL;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[self alloc] init];
+    });
+    return instance;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        handlers = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+- (void)registerForNotificationName:(NSString *)name callback:(void (^)(void))callback {
+    handlers[name] = callback;
+    CFNotificationCenterRef center = CFNotificationCenterGetDarwinNotifyCenter();
+    CFNotificationCenterAddObserver(center, (__bridge const void *)(self), defaultNotificationCallback, (__bridge CFStringRef)name, NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
+}
+
+- (void)postNotificationWithName:(NSString *)name {
+    CFNotificationCenterRef center = CFNotificationCenterGetDarwinNotifyCenter();
+    CFNotificationCenterPostNotification(center, (__bridge CFStringRef)name, NULL, NULL, YES);
+}
+
+- (void)notificationCallbackReceivedWithName:(NSString *)name {
+    void (^callback)(void) = handlers[name];
+    callback();
+}
+
+void defaultNotificationCallback (CFNotificationCenterRef center,
+                 void *observer,
+                 CFStringRef name,
+                 const void *object,
+                 CFDictionaryRef userInfo)
+{
+    NSLog(@"name: %@", name);
+    NSLog(@"userinfo: %@", userInfo);
+
+    NSString *identifier = (__bridge NSString *)name;
+    [[DarwinNotificationsManager sharedInstance] notificationCallbackReceivedWithName:identifier];
+}
+
+
+- (void)dealloc {
+    CFNotificationCenterRef center = CFNotificationCenterGetDarwinNotifyCenter();
+    CFNotificationCenterRemoveEveryObserver(center, (__bridge const void *)(self));
+}
+
 
 @end
